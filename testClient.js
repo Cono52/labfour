679dd479d94f86fd8c0cdb344c7b4d5e7bbfe849
Cono52 <oflanac52@gmail.com>
2016-11-20 10:10:36 +0000

    fix testclient message format

--- testClient.js
+++ testClient.js
@@ -8,14 +8,14 @@ let client = new net.Socket()
 let joinID
 
 let join = function() {
-    client.write("JOIN_CHATROOM:room1\n" +
+    client.write("JOIN_CHATROOM: room1\n" +
         "CLIENT_IP:192.127.1.6\n" +
         "PORT: 0\n" +
         "CLIENT_NAME:cono52\n")
 }
 
 let message = function() {
-    client.write("CHAT:room1\n" +
+    client.write("CHAT: room1\n" +
         "JOIN_ID:" + joinID + "\n" +
         "CLIENT_NAME:cono52\n" +
         "MESSAGE:Ullamcorper sem eu dui, curabitur vitae nulla, vulputate magna imperdiet at egestas.\n\n")
